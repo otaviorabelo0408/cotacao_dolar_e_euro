64b6e9a622a3a06a3f2988de256a96a55a088bff
Otávio Henrique de Freitas Rabelo <otaviorabelounb@gmail.com>
2021-12-06 19:58:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Arquivo com a função principal desse projeto:
 
-from utilidades.funcoes import atualiza, dolar, euro, sleep, system, d, e
+from utilidades.funcoes import (atualiza, sleep, system)
 
 if __name__ == '__main__':
     print("Sistema inicializando. Aguarde.")
     sleep(3)
     system("clear")
-    atualiza(dolar, euro, d, e)
+    atualiza()
