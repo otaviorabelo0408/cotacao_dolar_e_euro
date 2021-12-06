9b595e80fb8b98ecf78fea5aaafa794faf038370
Otávio Henrique de Freitas Rabelo <otaviorabelounb@gmail.com>
2021-12-06 19:39:31 -0300

    Update funcoes.py

--- utilidades/funcoes.py
+++ utilidades/funcoes.py
@@ -5,9 +5,9 @@ from requests import get
 from time import sleep
 from os import system
 from csv import writer
-from datetime import date, datetime
+from datetime import (date, datetime)
 from statistics import mean
-from matplotlib.pyplot import plot, ylabel, xlabel, title, show
+from matplotlib.pyplot import (plot, ylabel, xlabel, title, show, legend, axis)
 
 # Iniciando cotação:
 
@@ -18,6 +18,8 @@ euro = Euro(e)
 lista_dolar = list()
 lista_euro = list()
 lista_tempo = list()
+dolar_plotagem = list()
+euro_plotagem = list()
 lista_tempo.append(datetime.now().strftime("%H:%M:%S"))
 
 
@@ -26,33 +28,52 @@ lista_tempo.append(datetime.now().strftime("%H:%M:%S"))
 def atualiza(dolar_obj, euro_obj, d_1, e_1):
     """Função que imprime a primeira cotação
     catalogada e atualiza os valores das
-    cotações de acordo com a necessidade
-    do usuário."""
+    cotações."""
     if date.today().isoweekday() == 6 or date.today().isoweekday() == 7:
         print("Durante o fim de semana o mercado está fechado!")
         print("Encerrando programa. Aguarde 5 segundos.")
         sleep(5)
         exit(0)
     else:
-        print(f"Cotação do dólar: {dolar_obj.cotacao}")
-        print(f"Cotação do euro: {euro_obj.cotacao}")
-        lista_dolar.append(d_1)
-        lista_euro.append(e_1)
-        while datetime.now().strftime("%H:%M") != "17:00":
-            print("\nAguarde pela próxima atualização.")
-            for i in range(30, 0, -1):
-                sleep(1)
-            system("clear")
-            d_i = float(get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL").json()["USDBRL"]["bid"])
-            e_i = float(get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL").json()["EURBRL"]["bid"])
-            dolar_obj.cotacao = d_i
-            euro_obj.cotacao = e_i
-            lista_dolar.append(d_i)
-            lista_euro.append(e_i)
-            lista_tempo.append(datetime.now().strftime("%H:%M:%S"))
+        if int(datetime.now().strftime("%H")) < 10:
+            print("O mercado funciona apenas a partir das 10:00. Volte mais tarde!")
+            print("Encerrando programa. Aguarde 5 segundos.")
+            sleep(5)
+            exit(0)
+        elif int(datetime.now().strftime("%H")) > 17 or (
+                int(datetime.now().strftime("%H")) == 17 and int(datetime.now().strftime("%M")) != 0
+        ):
+            print("O mercado funciona apenas até às 17:00. Volte no próximo dia útil!")
+            print("Encerrando programa. Aguarde 5 segundos.")
+            sleep(5)
+            exit(0)
+        else:
             print(f"Cotação do dólar: {dolar_obj.cotacao}")
             print(f"Cotação do euro: {euro_obj.cotacao}")
-        menu()
+            lista_dolar.append(d_1)
+            lista_euro.append(e_1)
+            lista_tempo.append(datetime.now().strftime("%H:%M"))
+            dolar_plotagem.append(d_1)
+            euro_plotagem.append(e_1)
+            while datetime.now().strftime("%H:%M") != "17:00":
+                print("\nAguarde pela próxima atualização.")
+                for i in range(30, 0, -1):
+                    sleep(1)
+                system("clear")
+                d_i = float(get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL").json()["USDBRL"]["bid"])
+                e_i = float(get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL").json()["EURBRL"]["bid"])
+                dolar_obj.cotacao = d_i
+                euro_obj.cotacao = e_i
+                lista_dolar.append(d_i)
+                lista_euro.append(e_i)
+                if int(datetime.now().strftime("%M")) == 0 or int(datetime.now().strftime("%M")) == 30:
+                    if datetime.now().strftime("%H:%M") != lista_tempo[0]:
+                        lista_tempo.append(datetime.now().strftime("%H:%M"))
+                        dolar_plotagem.append(d_i)
+                        euro_plotagem.append(e_i)
+                print(f"Cotação do dólar: {dolar_obj.cotacao}")
+                print(f"Cotação do euro: {euro_obj.cotacao}")
+            menu()
 
 
 def arquiva_dados():
@@ -60,17 +81,20 @@ def arquiva_dados():
     em um arquivo do tipo .csv."""
     with open("/home/otavio/PycharmProjects/cotacao/cotacoes.csv", 'a') as arq:
         arq = writer(arq)
-        arq.writerow([date.today(), "Dólar", mean(lista_dolar)])
-        arq.writerow([date.today(), "Euro", mean(lista_euro)])
+        arq.writerow([date.today(), "Dólar", float(f"{mean(lista_dolar):.4f}")])
+        arq.writerow([date.today(), "Euro", float(f"{mean(lista_dolar):.4f}")])
 
 
-def plotagem(legenda, dados, nome):
+def plotagem(legenda, dados_1, dados_2):
     """Função que plota um gráfico de acordo com a
     solicitação do usuário."""
-    plot(legenda, dados, color='red')
+    plot(legenda, dados_1, color='red')
+    plot(legenda, dados_2, color='green')
     xlabel("Horário da cotação")
     ylabel("Valor alcançado")
-    title(f"Variação da cotação do {nome} com o tempo.")
+    title(f"Variação da cotação do dólar e do euro com o tempo.")
+    legend(title='Legenda:', loc='best')
+    axis('equal')
     show()
 
 
@@ -95,15 +119,13 @@ def menu():
                 sleep(5)
                 exit(0)
             elif opc == 2:
-                plotagem(lista_tempo, lista_dolar, "dólar")
-                plotagem(lista_tempo, lista_euro, "euro")
+                plotagem(lista_tempo, dolar_plotagem, euro_plotagem)
                 print("Aguarde 5 segundos pelo encerramento do programa.")
                 sleep(5)
                 exit(0)
             elif opc == 3:
                 arquiva_dados()
-                plotagem(lista_tempo, lista_dolar, "dólar")
-                plotagem(lista_tempo, lista_euro, "euro")
+                plotagem(lista_tempo, dolar_plotagem, euro_plotagem)
                 print("Dados arquivados com sucesso. Aguarde 5 segundos pelo encerramento do programa.")
                 sleep(5)
                 exit(0)
